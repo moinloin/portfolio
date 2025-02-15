5121d9c089b4ad0ba4993b548ab7e5a08b84c63c
Loïs Kauffungen <lois.kauffungen@gmail.com>
2025-02-15 13:07:58 +0100

    update server.js

--- server.js
+++ server.js
@@ -4,25 +4,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Statische Dateien ausliefern
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route für /
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Route für /cv
 app.get('/cv', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'cv.html'));
 });
 
-// 404 für alle anderen Routen
 app.use((req, res) => {
     res.status(404).send('not found');
 });
 
-// Server starten
 app.listen(PORT, () => {
     console.log(`Server running`);
 });
