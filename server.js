9b8c79a33dceccd338295977fdfb26f2e830111b
Loïs Kauffungen <lois.kauffungen@gmail.com>
2025-04-16 17:33:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/robots.txt', (req, res) => {
     res.type('text/plain');
-    res.sendFile(path.join(__dirname, 'public', 'robots.txt'));
+    res.sendFile(path.join(__dirname, 'public', 'views', 'robots.txt'));
 });
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
-
 app.get('/cv', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'cv.html'));
+    res.sendFile(path.join(__dirname, 'public', 'views', 'cv.html'));
 });
-
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'public', 'views', '404.html'));
 });
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
