460fec4f0763d3e860e638695d6635cab920f6f6
Loïs Kauffungen <lois.kauffungen@gmail.com>
2025-08-23 13:12:33 +0200

    fix issue with server start and app creation

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.get('/cv', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'views', 'cv.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get('/health', (req, res) => {
+  res.send('OK');
 });
+
+app.get('/version', (req, res) => {
+  res.json({ version: '1.0.0' });
+});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
